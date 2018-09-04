7ddb72c82aaade1963ed39866dcc907402ea20d2
cwwang15 <chaunecy@outlook.com>
2018-09-04 19:23:05 +0800

    添加了命令行读取的函数

--- lab6-cache/cachelab-handout/csim.c
+++ lab6-cache/cachelab-handout/csim.c
@@ -1,7 +1,63 @@
+#include <stdio.h>
 #include "cachelab.h"
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main()
+struct globalArgs_t {
+	int s;
+	int E;
+	int b;
+	int v;
+	int h;
+	char *t;
+	
+} globalArgs;
+
+
+/**
+ * 处理命令行参数。
+ * 没有处理参数不足的情况
+ */
+void parseCommand(int argc, char **argv, char *optstring) {
+	int opt = getopt(argc, argv, optstring);
+	while(opt != -1) {
+		switch(opt) {
+			case 'h':
+				globalArgs.h = 1;
+				break;
+			case 'v':
+				globalArgs.v = 1;
+				break;
+			case 's':
+				globalArgs.s = *optarg - '0';
+				break;
+			case 'E':
+				globalArgs.E = *optarg - '0';
+				break;
+			case 'b':
+				globalArgs.b = *optarg - '0';
+				break;
+			case 't':
+				globalArgs.t = optarg;
+				break;
+			default:
+				printf("unknown option, some error occurred");
+				exit(1);
+		}
+		
+		opt = getopt(argc, argv, optstring);
+	}
+}
+
+int main(int argc, char **argv)
 {
-    printSummary(0, 0, 0);
+	parseCommand(argc, argv, "s:E:b:t:vh?");
+	
+	printf("s: %d, E: %d, b: %d, t: %s, h: %d, v: %d\n", globalArgs.s, globalArgs.E, globalArgs.b, globalArgs.t, globalArgs.h, globalArgs.v);
+	
+    printSummary(9, 8, 6);
     return 0;
 }
+
+
